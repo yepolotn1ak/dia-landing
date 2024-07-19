ae761b91924643cd8fc559baf2f4a9659ff7ff23
Yevhenii <polotnyak2004@gmail.com>
2024-07-20 00:55:16 +0300

    clone dia landing

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# Dia Landing Page
+
+## Description
+This project is a landing page implementation based on the [Figma design](https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35).
+It uses BEM and SCSS for styling and adapts to various screen sizes.
+
+## Technologies Used
+- HTML
+- CSS (SCSS)
+- JavaScript
+- BEM Methodology
+- Figma for design
+
+## Links
+- **Preview:** [Live Demo](https://yepolotn1ak.github.io/layout_dia/)
+- **Mockup:** [Figma Design](https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35)
+
+## Launch Instructions
+1. **Fork** the repo.
+2. **Clone** the forked repo.
+3. Run `npm install` (or `npm i`).
+4. Run `npm start`.
+
+
 # Base layout template with Gulp, SCSS and Stylelint
 1. Create a repo using this template
 1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+let currentSlide = 1;
+const slides = document.querySelectorAll('.header__slider--photo');
+
+const showSlide = (index, needTransition = true) => {
+  slides.forEach(slide => {
+    slide.style.transition = needTransition ? 'transform .3s' : 'none';
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+};
+
+showSlide(currentSlide, false);
+
+const nextSlide = () => {
+  currentSlide++;
+  showSlide(currentSlide);
+
+  if (currentSlide >= slides.length - 1) {
+    setTimeout(() => {
+      currentSlide = 1;
+      showSlide(currentSlide, false);
+    }, 500);
+  }
+};
+
+const prevSlide = () => {
+  currentSlide--;
+  showSlide(currentSlide);
+
+  if (currentSlide === 0) {
+    setTimeout(() => {
+      currentSlide = slides.length - 2;
+      showSlide(currentSlide, false);
+    }, 500);
+  }
+};
+
+document.querySelector('.header__slider--button-left')
+  .addEventListener('click', prevSlide);
+
+document.querySelector('.header__slider--button-right')
+  .addEventListener('click', nextSlide);
+
+setInterval(nextSlide, 3500);
